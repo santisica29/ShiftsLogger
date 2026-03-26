fb87613c954de32cb395122b59196e78f9a62952
Santiago Sica <santisica29@gmail.com>
2026-03-26 20:57:17 -0300

    create interface for my service class

---
+++ ShiftsLogger/Interfaces/IShiftService.cs
@@ -0,0 +1,11 @@
+﻿using ShiftsLogger.Models;
+
+namespace ShiftsLogger.Interfaces;
+public interface IShiftService
+{
+    public List<Shift> GetAllShifts();
+    public Shift? GetShiftById(int id);
+    public Shift CreateShift(Shift Shift);
+    public Shift UpdateShift(int id, Shift updatedShift);
+    public string? DeleteShift(int id);
+}
