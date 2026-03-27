b24df28002e19b06e4ef70b124b7c1f6b2aa1c09
Santiago Sica <santisica29@gmail.com>
2026-03-26 21:17:52 -0300

    create the service class

--- ShiftsLogger/Program.cs
+++ ShiftsLogger/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using ShiftsLogger;
 using ShiftsLogger.Data;
+using ShiftsLogger.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddScoped<IShiftService, ShiftService>();
 builder.Services.AddDbContext<ShiftsLoggerDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
---
+++ ShiftsLogger/ShiftService.cs
@@ -0,0 +1,58 @@
+﻿using ShiftsLogger.Data;
+using ShiftsLogger.Interfaces;
+using ShiftsLogger.Models;
+
+namespace ShiftsLogger;
+
+public class ShiftService : IShiftService
+{
+    private readonly ShiftsLoggerDbContext _dbContext;
+
+    public ShiftService(ShiftsLoggerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Shift CreateShift(Shift Shift)
+    {
+        var savedShift = _dbContext.Shifts.Add(Shift);
+        _dbContext.SaveChanges();
+        return savedShift.Entity;
+    }
+
+    public string? DeleteShift(int id)
+    {
+        Shift? savedShift = _dbContext.Shifts.Find(id);
+
+        if (savedShift == null)
+            return null;
+
+        _dbContext.Shifts.Remove(savedShift);
+        _dbContext.SaveChanges();
+
+        return $"Successfully deleted shift with id {id}";
+    }
+
+    public List<Shift> GetAllShifts()
+    {
+        return _dbContext.Shifts.ToList();
+    }
+
+    public Shift? GetShiftById(int id)
+    {
+        return _dbContext.Shifts.Find(id);
+    }
+
+    public Shift UpdateShift(int id, Shift updatedShift)
+    {
+        Shift? savedShift = _dbContext.Shifts.Find(id);
+
+        if (savedShift == null)
+            return null;
+
+        _dbContext.Entry(savedShift).CurrentValues.SetValues(updatedShift);
+        _dbContext.SaveChanges();
+
+        return savedShift;
+    }
+}
