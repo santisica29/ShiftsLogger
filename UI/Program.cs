3870451ddad95f810c9ec759f49b0bdcf582e639
Santiago Sica <santisica29@gmail.com>
2026-04-01 00:12:30 -0300

    Add UI project to this solution

---
+++ UI/Program.cs
@@ -0,0 +1,11 @@
+﻿await Task.Delay(2000);
+
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:7263");
+
+var response = await client.GetAsync("/api/shifts");
+
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
+Console.ReadLine();
