85309f2ff1090d9b65813107f4ac2af0164cd549
Santiago Sica <santisica29@gmail.com>
2026-04-01 01:13:03 -0300

    Add async method

---
+++ UI/ApiService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace UI;
+
+internal class ApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public ApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<T?> GetAsync<T>(string url)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+
+        string json = await response.Content.ReadAsStringAsync();
+
+        T? result = JsonSerializer.Deserialize<T>(json);
+
+        return result;
+    }
+}
