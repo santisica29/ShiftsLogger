2972b16ae7498496960bd9de81ccd5afdb0fa6ed
Santiago Sica <santisica29@gmail.com>
2026-04-03 22:31:08 -0300

    start service class

---
+++ UI/ShiftService.cs
@@ -0,0 +1,16 @@
+﻿namespace UI;
+
+internal class ShiftService
+{
+    private readonly ApiService _apiService;
+
+    public ShiftService(ApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
+    public async Task<Shift> GetShift()
+    {
+        var response = await _apiService.GetAsync<Shift>()
+    }
+}
